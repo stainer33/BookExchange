07ba9aee7570551e5fcd2d1a6e370e554ed66aab
sujitkhadgi <sujitkhadgi12@gmail.com>
2020-06-24 11:03:36 +0545

    Login and token test case

---
+++ app/src/test/java/com/example/bookexchange1/LoginTest.java
@@ -0,0 +1,10 @@
+package com.example.bookexchange1;
+
+public class LoginTest {
+//    @Test
+//    public void testLogin(){
+//        LoginBLL loginBLL = new LoginBLL();
+//        boolean result = loginBLL.checkUser("username", "Admin123");
+//        assertEquals(true, result);
+//    }
+}
---
+++ app/src/test/java/com/example/bookexchange1/TokenTest.java
@@ -0,0 +1,19 @@
+package com.example.bookexchange1;
+
+public class TokenTest {
+//    @Test
+//    public void testToken() {
+//
+//
+//        UserAPI userAPI = URL.getInstance().create(UserAPI.class);
+//        Call<LoginResponse> usersCall = userAPI.userLogin("username", "Admin123");
+//
+//        try {
+//            Response<LoginResponse> loginResponse = usersCall.execute();
+//            Url.token = "Bearer " + loginResponse.body().getToken();
+//            assertThat(Url.token, is(IsNull.notNullValue()));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+}
