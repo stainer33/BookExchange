013ac37d4a2346a6ef29c315d6ee60ec533a3622
stainer33 <spasang91@gmail.com>
2020-06-25 13:03:01 +0545

    bottom navigation added

---
+++ app/src/main/java/Fragments/HomeFragment.java
@@ -0,0 +1,34 @@
+package Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bookexchange1.R;
+
+
+public class HomeFragment extends Fragment {
+
+
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/bookexchange1/UI/DashboardActivity.java
@@ -0,0 +1,71 @@
+package com.example.bookexchange1.UI;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.bookexchange1.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import Fragments.HomeFragment;
+import Fragments.MyBooksFragment;
+import Fragments.NotificationFragment;
+import Fragments.ProfileFragment;
+
+public class DashboardActivity extends AppCompatActivity {
+BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        bottomNavigationView=findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        loadFragment(new HomeFragment());
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.nav_home:
+                     //   toolbar.setTitle("Home");
+                        fragment = new HomeFragment();
+                        loadFragment(fragment);
+                        return true;
+                    case R.id.nav_myBooks:
+                      //  toolbar.setTitle("Register");
+                        fragment = new MyBooksFragment();
+                        loadFragment(fragment);
+                        return true;
+                    case R.id.nav_notification:
+                        //toolbar.setTitle("About us");
+                        fragment = new NotificationFragment();
+                        loadFragment(fragment);
+                        return true;
+
+                    case R.id.nav_profile:
+                        //toolbar.setTitle("About us");
+                        fragment = new ProfileFragment();
+                        loadFragment(fragment);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
+    }
+
+
+    private void loadFragment(Fragment fragment) {
+        // load fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout, fragment);
+        transaction.commit();
+    }
+
+}
